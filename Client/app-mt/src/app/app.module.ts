f6aa75063fd69d6ed66539a6d2ba155aa9f129e6
Сергей Еремеенков <shauf1995@gmail.com>
2023-02-19 03:15:19 +0200

    TX-7 add start page

--- Client/app-mt/src/app/app.module.ts
+++ Client/app-mt/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecordComponent } from './record/record.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecordComponent
+    RecordComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
