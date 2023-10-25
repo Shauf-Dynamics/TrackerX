b6083f0913b2cd2b8d3408e18c15d3cb7ab3b25a
Сергей Еремеенков <shauf1995@gmail.com>
2023-10-25 21:32:51 +0300

    TRAC-19 basic angular app

---
+++ Client/trackerx-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SideBarComponent } from './modules/side-bar/side-bar.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideBarComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
