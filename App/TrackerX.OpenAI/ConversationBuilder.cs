2b60c01f723ff3b7a915178d7d160dab424c7bfe
Сергей Еремеенков <shauf1995@gmail.com>
2024-01-04 02:37:40 +0200

    TRAC-45 add basic OpenAI project

---
+++ App/TrackerX.OpenAI/ConversationBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+
+namespace TrackerX.OpenAI
+{
+    internal static class ConversationBuilder
+    {
+        private static Model OpenAIMode = Model.ChatGPTTurbo;
+        private static double Temperature = 0;
+
+        internal static Conversation Build(IConfiguration config)
+        {
+            var apiToken = config["OpenAI:ApiToken"];
+            var api = new OpenAIAPI(new APIAuthentication(apiToken));
+
+            Conversation chat = api.Chat.CreateConversation();
+            chat.Model = OpenAIMode;
+            chat.RequestParameters.Temperature = Temperature;
+
+            return chat;
+        }
+    }
+}
---
+++ App/TrackerX.OpenAI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerX.OpenAI
+{
+    class Program
+    {
+        public async static Task Main()
+        {
+            var chat = ConversationBuilder.Build(null);
+
+            chat.AppendSystemMessage("This conversation is about music." +
+                "User will requests certain information." +
+                "This information should be responded as a list. " +
+                "Any additional text should be droped.");
+            
+            chat.AppendUserInput("List top 10 'Deep Purple' songs along with albums each song belongs. Add bpm for each song if possible");
+
+            await chat.StreamResponseFromChatbotAsync(res =>
+            {
+                try
+                {
+                    Console.Write(res);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }               
+            });
+
+            Console.ReadKey();
+        }
+    }   
+}
