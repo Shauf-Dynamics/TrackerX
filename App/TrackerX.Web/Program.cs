d7cdac52d0f3e81f12eabf5bf3b3f3a6c425a0a5
shauf <shauf@gmail.com>
2024-01-15 18:35:44 +0200

    TRAC-49 simplify startup in web project

--- App/TrackerX.Web/Program.cs
+++ App/TrackerX.Web/Program.cs
@@ -1,4 +1,38 @@
 using TrackerX.Web;
+using TrackerX.Web.Moduls;
+using TrackerX.Cryptography;
 
-var startup = new Startup(args);
-startup.ConfigureAndRun();
+var builder = WebApplication.CreateBuilder(args);
+
+var allowSpecificOrigins = "_allowSpecificOrigins";
+builder.Services.AddCustomCors(allowSpecificOrigins);
+
+builder.Services.AddCryptographyServices();
+builder.Services.AddBusinessServices();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuth();
+
+builder.Services.AddDataAccess(builder.Configuration.GetConnectionString("dev"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(allowSpecificOrigins);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
