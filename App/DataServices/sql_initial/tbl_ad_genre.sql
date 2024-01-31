388848b09153eed3cfada66a61836ff545ffca0e
shauf <shauf@gmail.com>
2024-01-31 23:11:13 +0200

    TRAC-55 tbl_ad_genre fixes

--- App/DataServices/sql_initial/tbl_ad_genre.sql
+++ App/DataServices/sql_initial/tbl_ad_genre.sql
@@ -3,9 +3,14 @@ USE [db_TrackerX]
 IF (NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'tbl_ad_genre'))
 BEGIN
     CREATE TABLE tbl_ad_genre (
-        genre_id        INT IDENTITY(1,1) PRIMARY KEY,
-        genre_name      NVARCHAR(64),
-        genre_parent_id INT
+        genre_id            INT IDENTITY(1,1) PRIMARY KEY,
+        genre_name          NVARCHAR(64),
+        genre_parent_id     INT,
+        created_dttm        DATETIME,
+        modified_dttm       DATETIME,
+        created_by_user_id  INT,
+        modified_by_user_id INT,
+        deleled_ind         BIT DEFAULT(0)
     )
 
     ALTER TABLE tbl_ad_genre
